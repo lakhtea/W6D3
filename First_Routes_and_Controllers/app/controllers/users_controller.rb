7b5bdfecd99bfceae7608e2dca69c75a2d51bc4b
Lakhte <lakhteagha@gmail.com>
2021-02-03 15:33:24 -0500

    users_controller and table

--- First_Routes_and_Controllers/app/controllers/users_controller.rb
+++ First_Routes_and_Controllers/app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+  
+  def create
+    user = User.new(user_params)
+    user.save!
+    render json: user
+  end
 
+  def show
+    render json: user
+  end
 
-
+  def user_params
+        params.require(:user).permit(:name, :email)
+    end
 end
