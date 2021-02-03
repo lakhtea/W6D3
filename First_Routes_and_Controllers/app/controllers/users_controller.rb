d77b76c091348bd9df3cd1774fd615811b16a841
Alisa Cheadle <alisa1649@gmail.com>
2021-02-03 16:11:00 -0500

    Added update and destroy methods

--- First_Routes_and_Controllers/app/controllers/users_controller.rb
+++ First_Routes_and_Controllers/app/controllers/users_controller.rb
@@ -11,7 +11,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    user = User.find(params[:id])
     render json: user
+  end
+
+  def update 
+    user = User.find(params[:id])
+    if user.update(user_params)
+        redirect_to user_url(user)
+    else
+        render json: user.errors.full_message, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  user = User.find(params[:id])
+  user.destroy
+#   redirect_to user_url
   end
 
   def user_params
